39d802657b86e7caf5446d8bd2b1a16b1d2d8ebf
AlfredLajeunesse <lajeunessea@seminaire-sherbrooke.qc.ca>
2025-12-09 16:56:09 -0500

    remove fuck git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewGrosBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewGrosBot.java
@@ -157,7 +157,7 @@ public class NewGrosBot extends LinearOpMode {
             } else if (gamepad2.a) {
                 servo1.setPower(-1);
             } else if (gamepad2.x) {
-                servo1.setPower(0);//fuck git
+                servo1.setPower(0);
             }
 
             if (gamepad2.right_trigger > 0.75) {
